8423aae59e33af84879e1e6340406c31e8c4118f
KevinJCHe <kevinjc.he@gmail.com>
2025-05-14 21:11:44 -0400

    updated paths

--- vec_inf/client/slurm_vars.py
+++ vec_inf/client/slurm_vars.py
@@ -7,8 +7,8 @@ from typing_extensions import Literal
 
 CACHED_CONFIG = Path("/", "model-weights", "vec-inf-shared", "models_latest.yaml")
 LD_LIBRARY_PATH = "/scratch/ssd001/pkgs/cudnn-11.7-v8.5.0.96/lib/:/scratch/ssd001/pkgs/cuda-11.7/targets/x86_64-linux/lib/"
-SINGULARITY_IMAGE = "/model-weights/vec-inf-shared/vector-inference_latest.sif"
-SINGULARITY_LOAD_CMD = "module load singularity-ce/3.8.2"
+SINGULARITY_IMAGE = "/cluster/projects/gliugroup/2BLAST/containers/vec-inf-image.sif"
+SINGULARITY_LOAD_CMD = "module load singularity/3.11.0"
 VLLM_NCCL_SO_PATH = "/vec-inf/nccl/libnccl.so.2.18.1"
 MAX_GPUS_PER_NODE = 8
 MAX_NUM_NODES = 16
