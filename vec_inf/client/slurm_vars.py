97443570472832e248305b51b0b4377b6d0a6a0c
KevinJCHe <kevinjc.he@gmail.com>
2025-06-06 20:10:59 -0400

    updated models.yaml to our specific  models

--- vec_inf/client/slurm_vars.py
+++ vec_inf/client/slurm_vars.py
@@ -7,7 +7,7 @@ from typing_extensions import Literal
 
 CACHED_CONFIG = Path("/", "model-weights", "vec-inf-shared", "models_latest.yaml")
 LD_LIBRARY_PATH = "/scratch/ssd001/pkgs/cudnn-11.7-v8.5.0.96/lib/:/scratch/ssd001/pkgs/cuda-11.7/targets/x86_64-linux/lib/"
-SINGULARITY_IMAGE = "/cluster/projects/gliugroup/2BLAST/containers/vec-inf-image.sif"
+SINGULARITY_IMAGE = "/cluster/projects/gliugroup/2BLAST/containers/vec-inf-image-2025-05-15.sif"
 SINGULARITY_LOAD_CMD = "module load singularity/3.11.0"
 VLLM_NCCL_SO_PATH = "/vec-inf/nccl/libnccl.so.2.18.1"
 MAX_GPUS_PER_NODE = 8
